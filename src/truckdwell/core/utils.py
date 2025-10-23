24cd92a42eb9d56736ec798cdb4cc4ab7483d493
adrlm <@>
2025-10-23 14:14:38 -0300

    feat: criado a função limpar dados

--- src/truckdwell/core/utils.py
+++ src/truckdwell/core/utils.py
@@ -1,1 +1,24 @@
+from tkinter import END, filedialog, messagebox
+from src.truckdwell.pdf.extrator import extrair_dados_pdf
+from src.truckdwell.planilha.excel import preencher_planilha
+
+def limparDados(self):
+    campos = [
+        self.inputTransportadora,
+        self.inputNF,
+        self.inputProduto,
+        self.inputPeso,
+        self.inputDataHoraSaida,
+        self.inputDataHoraChegada,
+        self.inputFornecedor,
+        self.inputCte,
+        self.inputMotorista,
+        self.inputMotivo
+    ]
+
+    for campo in campos:
+        campo.delete(0,END)
+
+    self.inputFornecedor.focus()  # Puxa o foco para o campo Fornecedor
+
 
