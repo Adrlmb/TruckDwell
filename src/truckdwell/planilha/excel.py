83ee5fa93449944a0856807f0c1b0858be6cf761
adrlm <@>
2025-11-10 21:07:29 -0300

    backup: deletar arquivos desnecessários

---
+++ src/truckdwell/planilha/excel.py
@@ -0,0 +1,21 @@
+from openpyxl import load_workbook
+
+def preencher_planilha(caminho_modelo, dados, caminho_saida):
+    wb = load_workbook(caminho_modelo)
+    planilha = wb.active
+
+    planilha['F3'] = dados['transportadora']
+    planilha['C4'] = int(dados['nf'])
+    planilha['C5'] = dados['produto']
+    planilha['F12'] = dados['peso']
+    planilha['B15'] = dados['dataHoraSaida']
+
+    planilha['B9'] = dados['dataHoraChegada']
+    planilha['C3'] = dados['fornecedor']
+    planilha['F4'] = dados['cte']
+    planilha['F5'] = dados['motorista']
+    planilha['E16'] = "MOTIVO: " + dados['motivo']
+
+    wb.save(caminho_saida)
+    wb.close()
+
